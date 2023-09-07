91c1dcd4c10315fccddde91ff16459c13508dfcf
vladgatu <gatu.vlad2010@gmail.com>
2023-09-07 17:27:08 +0300

    Update socials

--- js/main.js
+++ js/main.js
@@ -60,6 +60,32 @@
   
 // });
 
+// //Get the button
+// let mybutton = document.getElementById("btn-back-to-top");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () {
+// scrollFunction();
+// };
+
+// function scrollFunction() {
+// if (
+// document.body.scrollTop > 20 ||
+// document.documentElement.scrollTop > 20
+// ) {
+// mybutton.style.display = "block";
+// } else {
+// mybutton.style.display = "none";
+// }
+// }
+// // When the user clicks on the button, scroll to the top of the document
+// mybutton.addEventListener("click", backToTop);
+
+// function backToTop() {
+// document.body.scrollTop = 0;
+// document.documentElement.scrollTop = 0;
+// }
+
 (function ($) {
   "use strict";
   $(document).ready(function () {
@@ -101,6 +127,17 @@
 
     $('[data-bs-toggle="tooltip"]').on('click', function () {
       $(this).tooltip('hide')
+    });
+
+    $(window).on('scroll', function() {
+      const scrollTop = document.getElementById('btn-back-to-top');
+      if (window.scrollY > 200) {
+        scrollTop.style.visibility = "visible";
+          scrollTop.style.opacity = 1;
+        } else {
+          scrollTop.style.visibility = "hidden";
+          scrollTop.style.opacity = 0;
+        }
     });
   });
 })(jQuery); 
