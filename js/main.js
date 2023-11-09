92297e2c2aa8975f1124180ab4bf630242ceda63
vladgatu <gatu.vlad2010@gmail.com>
2023-11-10 00:41:36 +0200

    Delete files & add en vers & updates

--- js/main.js
+++ js/main.js
@@ -90,74 +90,74 @@
   "use strict";
   $(document).ready(function () {
     // --- FILL WORK CAROUSEL FUNCTIONALITY ---
-    tgFreeWork.content.forEach(element => {
+    // tgFreeWork.content.forEach(element => {
 
-      let contentText = element.text;
-      if (contentText.length >= 250) { // max item post character count
-        contentText = contentText.substring(0, 250 - 3);
-        contentText += '...';
-      }
+    //   let contentText = element.text;
+    //   if (contentText.length >= 250) { // max item post character count
+    //     contentText = contentText.substring(0, 250 - 3);
+    //     contentText += '...';
+    //   }
 
-      const elemContent =
-        `<div class="item" data-bs-placement="top" data-bs-toggle="tooltip" title="${tgFreeWork.tooltip}">
-          <a href="${element.link}" target="_blank">
-            <div class="col-md-12 wow fadeInUp ">
-              <div class="main_services-work text-center" >
-                <img class="round_picture_img" src="${element.img}">
-                <h3 class="mt-3"> 
-                  ${element.title}
-                </h3>
-                <p>
-                  ${contentText} 
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>`;
+    //   const elemContent =
+    //     `<div class="item" data-bs-placement="top" data-bs-toggle="tooltip" title="${tgFreeWork.tooltip}">
+    //       <a href="${element.link}" target="_blank">
+    //         <div class="col-md-12 wow fadeInUp ">
+    //           <div class="main_services-work text-center" >
+    //             <img class="round_picture_img" src="${element.img}">
+    //             <h3 class="mt-3"> 
+    //               ${element.title}
+    //             </h3>
+    //             <p>
+    //               ${contentText} 
+    //             </p>
+    //           </div>
+    //         </div>
+    //       </a>
+    //     </div>`;
 
-      $('#owl-carousel-container-work').trigger('add.owl.carousel', elemContent);
-      $('#owl-carousel-container-work').trigger('refresh.owl.carousel');
-    });
+    //   $('#owl-carousel-container-work').trigger('add.owl.carousel', elemContent);
+    //   $('#owl-carousel-container-work').trigger('refresh.owl.carousel');
+    // });
 
-    tgFreeWork.content.forEach(element => {
+    // tgFreeWork.content.forEach(element => {
 
-      let contentText = element.text;
-      if (contentText.length >= 250) { // max item post character count
-        contentText = contentText.substring(0, 250 - 3);
-        contentText += '...';
-      }
+    //   let contentText = element.text;
+    //   if (contentText.length >= 250) { // max item post character count
+    //     contentText = contentText.substring(0, 250 - 3);
+    //     contentText += '...';
+    //   }
 
-      const elemContent =
-        `<div class="item" data-bs-placement="top" data-bs-toggle="tooltip" title="${tgFreeWork.tooltip}">
-          <a href="${element.link}" target="_blank">
-            <div class="col-md-12 wow fadeInUp ">
-              <div class="main_services-posts text-center" >
-                <img class="round_picture_img" src="${element.img}">
-                <h3 class="mt-3"> 
-                  ${element.title}
-                </h3>
-                <p>
-                  ${contentText} 
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>`;
+    //   const elemContent =
+    //     `<div class="item" data-bs-placement="top" data-bs-toggle="tooltip" title="${tgFreeWork.tooltip}">
+    //       <a href="${element.link}" target="_blank">
+    //         <div class="col-md-12 wow fadeInUp ">
+    //           <div class="main_services-posts text-center" >
+    //             <img class="round_picture_img" src="${element.img}">
+    //             <h3 class="mt-3"> 
+    //               ${element.title}
+    //             </h3>
+    //             <p>
+    //               ${contentText} 
+    //             </p>
+    //           </div>
+    //         </div>
+    //       </a>
+    //     </div>`;
 
-      $('#owl-carousel-container-posts').trigger('add.owl.carousel', elemContent);
-      $('#owl-carousel-container-posts').trigger('refresh.owl.carousel');
-    });
+    //   $('#owl-carousel-container-posts').trigger('add.owl.carousel', elemContent);
+    //   $('#owl-carousel-container-posts').trigger('refresh.owl.carousel');
+    // });
 
-    $('[data-bs-toggle="tooltip"]').tooltip();
-    $('[data-bs-toggle="tooltip"]').click(_ => {
-      console.log('CLICK_1'); // TODO :: remove this shit 
-      $('[data-bs-toggle="tooltip"]').tooltip('hide');
-    });
-    $('[data-bs-toggle="tooltip"]').tooltip({ trigger: "hover" });
+    // $('[data-bs-toggle="tooltip"]').tooltip();
+    // $('[data-bs-toggle="tooltip"]').click(_ => {
+    //   console.log('CLICK_1'); // TODO :: remove this shit 
+    //   $('[data-bs-toggle="tooltip"]').tooltip('hide');
+    // });
+    // $('[data-bs-toggle="tooltip"]').tooltip({ trigger: "hover" });
 
-    $('[data-bs-toggle="tooltip"]').on('click', function () {
-      $(this).tooltip('hide')
-    });
+    // $('[data-bs-toggle="tooltip"]').on('click', function () {
+    //   $(this).tooltip('hide')
+    // });
 
 
     // --- PROGRESS & BACK TO TOP BUTTON FUNCTIONALITY ---
@@ -198,7 +198,7 @@
 			}
 		});				
 		jQuery('.progress-wrap').on('click', function(event) {
-      console.log('PULA');
+      // console.log('UP_CLICK');
 			// event.preventDefault();
 			// jQuery('html, body').animate({scrollTop: 0}, duration);
       // document.getElementById('tg_about').scrollIntoView({behavior: 'smooth'});
